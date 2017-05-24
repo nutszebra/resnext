646b9b61cbf1b483f3f651bfbbe5ecb144a8a44a
nutszebra <nutszebra@gmail.com>
2017-05-24 19:50:54 +0900

    pretrained model converter

---
+++ ilsvrc_converter.py
@@ -0,0 +1,197 @@
+import six
+import argparse
+import resnext_ilsvrc
+from torch.utils.serialization import load_lua
+
+
+def judge_component(component, which):
+    if which in str(type(component)):
+        return True
+    return False
+
+
+def judge_bn(component):
+    return judge_component(component, 'SpatialBatchNormalization')
+
+
+def judge_linear(component):
+    return judge_component(component, 'nn.Linear.Linear')
+
+
+def judge_torch_obj(component):
+    return judge_component(component, 'TorchObject')
+
+
+def judge_relu(component):
+    return judge_torch_obj(component) and 'cndnn.ReLU' in component._typename
+
+
+def judge_conv(component):
+    return judge_torch_obj(component) and 'SpatialConvolution' in component._typename
+
+
+def extract_bn(component):
+    parameters = {}
+    parameters['mean'] = component.__dict__['running_mean'].numpy()
+    parameters['var'] = component.__dict__['running_var'].numpy()
+    parameters['gamma'] = component.__dict__['weight'].numpy()
+    parameters['beta'] = component.__dict__['bias'].numpy()
+    parameters['whoami'] = 'bn {}'.format(parameters['beta'].shape)
+    return parameters
+
+
+def extract_conv(component):
+    parameters = {'dH': 1, 'padH': 0}
+    if 'bias' in component._obj:
+        parameters['bias'] = component._obj['bias'].numpy()
+    if 'weight' in component._obj:
+        parameters['weight'] = component._obj['weight'].numpy()
+    if 'groups' in component._obj:
+        parameters['groups'] = component._obj['groups']
+    if 'dH' in component._obj:
+        parameters['dH'] = component._obj['dH']
+    if 'padH' in component._obj:
+        parameters['padH'] = component._obj['padH']
+    parameters['whoami'] = 'conv {} {} {} {}'.format(parameters['weight'].shape, parameters['groups'], parameters['dH'], parameters['padH'])
+    return parameters
+
+
+def extract_linear(component):
+    parameters = {}
+    if 'bias' in component.__dict__:
+        parameters['bias'] = component.__dict__['bias'].numpy()
+    if 'weight' in component.__dict__:
+        parameters['weight'] = component.__dict__['weight'].numpy()
+    parameters['whoami'] = 'linear {}'.format(parameters['weight'].shape)
+    return parameters
+
+
+def copy_linear(model, dic):
+    if 'bias' in dic:
+        assert model.b.data.shape == dic['bias'].shape
+        assert model.b.data.dtype == dic['bias'].dtype
+        model.b.data = dic['bias']
+    if 'weight' in dic:
+        assert model.W.data.shape == dic['weight'].shape
+        assert model.W.data.dtype == dic['weight'].dtype
+        model.W.data = dic['weight']
+
+
+def copy_conv(model, dic):
+    if 'dH' in dic:
+        assert model.stride[0] == dic['dH']
+    if 'padH' in dic:
+        assert model.pad[0] == dic['padH']
+    if 'bias' in dic:
+        assert model.b.data.shape == dic['bias'].shape
+        assert model.b.data.dtype == dic['bias'].dtype
+        model.b.data = dic['bias']
+    if 'weight' in dic:
+        assert model.W.data.shape == dic['weight'].shape
+        assert model.W.data.dtype == dic['weight'].dtype
+        model.W.data = dic['weight']
+
+
+def copy_bn(model, dic):
+    if 'mean' in dic:
+        assert model.avg_mean.shape == dic['mean'].shape
+        assert model.avg_mean.dtype == dic['mean'].dtype
+        model.avg_mean = dic['mean']
+    if 'var' in dic:
+        assert model.avg_var.shape == dic['var'].shape
+        assert model.avg_var.dtype == dic['var'].dtype
+        model.avg_var = dic['var']
+    if 'gamma' in dic:
+        assert model.gamma.data.shape == dic['gamma'].shape
+        assert model.gamma.data.dtype == dic['gamma'].dtype
+        model.gamma.data = dic['gamma']
+    if 'beta' in dic:
+        assert model.beta.data.shape == dic['beta'].shape
+        assert model.beta.data.dtype == dic['beta'].dtype
+        model.beta.data = dic['beta']
+
+
+def copy_group_conv(model, dic):
+    if 'groups' in dic:
+        assert model.groups == dic['groups']
+        if 'weight' in dic:
+            branch_width = int(dic['weight'].shape[0] / dic['groups'])
+            for i in six.moves.range(dic['groups']):
+                m = model['conv_group_{}'.format(i)].conv
+                assert m.W.data.shape == dic['weight'][i * branch_width: i * branch_width + branch_width].shape
+                m.W.data = dic['weight'][i * branch_width: i * branch_width + branch_width]
+        if 'bias' in dic:
+            branch_width = int(dic['bias'].shape[0] / dic['groups'])
+            for i in six.moves.range(dic['groups']):
+                m = model['conv_group_{}'.format(i)].conv
+                assert m.b.data.shape == dic['bias'][i * branch_width: i * branch_width + branch_width].shape
+                m.b.data = dic['bias'][i * branch_width: i * branch_width + branch_width]
+
+
+def copy_conv_bn_relu(model, parameters):
+    copy_conv(model.conv, parameters[0])
+    copy_bn(model.bn, parameters[1])
+
+
+def copy_bn_relu(model, dic):
+    copy_bn(model.bn, dic)
+
+
+def copy_conv_bn(model, parameters):
+    copy_conv_bn_relu(model, parameters)
+
+
+def copy_resnext_block(block, parameters, skip_connection=None):
+    copy_conv_bn_relu(block.conv_bn_relu_1, [parameters[0], parameters[1]])
+    copy_group_conv(block.conv_group_2, parameters[2])
+    copy_bn_relu(block.bn_relu_3, parameters[3])
+    copy_conv_bn(block.conv_bn_4, [parameters[4], parameters[5]])
+    if skip_connection is not None:
+        copy_conv_bn(skip_connection, [parameters[6], parameters[7]])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='convert torch resnext model to chainer resnext model')
+    parser.add_argument('--t7', '-t',
+                        default=str,
+                        help='t7 file')
+    args = parser.parse_args().__dict__
+    t7 = args['t7']
+    print(args)
+    print('start convering')
+    trained_model = load_lua(t7, unknown_classes=True)
+    components = []
+
+    def dfs(modules):
+        try:
+            for module in modules.modules:
+                dfs(module)
+        except:
+            components.append(modules)
+    dfs(trained_model)
+
+    parameters = []
+    for c in components:
+        for judge, extract in ((judge_bn, extract_bn), (judge_linear, extract_linear), (judge_conv, extract_conv)):
+            if judge(c):
+                parameters.append(extract(c))
+                break
+    if '101_64x4d' in t7:
+        model = resnext_ilsvrc.ResNext(1000, block_num=(3, 4, 23, 3), C=64, d=4)
+        count = 0
+        copy_conv_bn_relu(model.conv_bn_relu, parameters[count: count + 2])
+        count += 2
+        for i, b in enumerate([3, 4, 23, 3]):
+            print('converting block {}'.format(i))
+            for ii in six.moves.range(b):
+                name = 'resnext_block_{}_{}'.format(i, ii)
+                print('    converting {}'.format(name))
+                if ii == 0:
+                    copy_resnext_block(model[name], parameters[count: count + 8], skip_connection=model['skip_connection_{}'.format(i)])
+                    count += 8
+                else:
+                    copy_resnext_block(model[name], parameters[count: count + 6])
+                    count += 6
+        print('linear')
+        copy_linear(model.linear.linear, parameters[count])
+    print('finished convering')
---
+++ resnext_ilsvrc.py
@@ -0,0 +1,256 @@
+import six
+import numpy as np
+import chainer.links as L
+import chainer.functions as F
+import nutszebra_chainer
+import functools
+from collections import defaultdict
+
+
+class Linear(nutszebra_chainer.Model):
+
+    def __init__(self, in_channel, out_channel):
+        super(Linear, self).__init__(
+            linear=L.Linear(in_channel, out_channel),
+        )
+        self.in_channel = in_channel
+        self.out_channel = out_channel
+
+    def weight_initialization(self):
+        self.linear.W.data = self.weight_relu_initialization(self.linear)
+        self.linear.b.data = self.bias_initialization(self.linear, constant=0)
+
+    def count_parameters(self):
+        return functools.reduce(lambda a, b: a * b, self.linear.W.data.shape)
+
+    def __call__(self, x, train=False):
+        return self.linear(x)
+
+
+class Conv(nutszebra_chainer.Model):
+
+    def __init__(self, in_channel, out_channel, filter_size=(3, 3), stride=(1, 1), pad=(1, 1)):
+        super(Conv, self).__init__(
+            conv=L.Convolution2D(in_channel, out_channel, filter_size, stride, pad),
+        )
+        self.in_channel = in_channel
+        self.out_channel = out_channel
+
+    def weight_initialization(self):
+        self.conv.W.data = self.weight_relu_initialization(self.conv)
+        self.conv.b.data = self.bias_initialization(self.conv, constant=0)
+
+    def count_parameters(self):
+        return functools.reduce(lambda a, b: a * b, self.conv.W.data.shape)
+
+    def __call__(self, x, train=False):
+        return self.conv(x)
+
+
+class Group_Conv(nutszebra_chainer.Model):
+
+    def __init__(self, in_channel, out_channel, filter_size=(3, 3), stride=(1, 1), pad=(1, 1), groups=32):
+        super(Group_Conv, self).__init__()
+        modules = []
+        for i in six.moves.range(groups):
+            modules += [('conv_group_{}'.format(i), Conv(in_channel, out_channel, filter_size, stride, pad))]
+        # register layers
+        [self.add_link(*link) for link in modules]
+        self.modules = modules
+        self.in_channel = in_channel
+        self.out_channel = out_channel
+        self.filter_size = filter_size
+        self.stride = stride
+        self.pad = pad
+        self.groups = groups
+
+    def weight_initialization(self):
+        [link.weight_initialization() for _, link in self.modules]
+
+    def count_parameters(self):
+        return int(np.sum([link.count_parameters() for _, link in self.modules]))
+
+    def __call__(self, x, train=False):
+        s = self.in_channel
+        h = [self['conv_group_{}'.format(i)](x[:, i * s: i * s + s], train=train) for i in six.moves.range(self.groups)]
+        return F.concat(h, axis=1)
+
+
+class BN_ReLU(nutszebra_chainer.Model):
+
+    def __init__(self, in_channel):
+        super(BN_ReLU, self).__init__(
+            bn=L.BatchNormalization(in_channel),
+        )
+        self.in_channel = in_channel
+
+    def weight_initialization(self):
+        pass
+
+    def __call__(self, x, train=False):
+        return F.relu(self.bn(x, test=not train))
+
+    def count_parameters(self):
+        return 0
+
+
+class Conv_BN(nutszebra_chainer.Model):
+
+    def __init__(self, in_channel, out_channel, filter_size=(3, 3), stride=(1, 1), pad=(1, 1)):
+        super(Conv_BN, self).__init__(
+            conv=L.Convolution2D(in_channel, out_channel, filter_size, stride, pad),
+            bn=L.BatchNormalization(out_channel),
+        )
+        self.in_channel = in_channel
+        self.out_channel = out_channel
+
+    def weight_initialization(self):
+        self.conv.W.data = self.weight_relu_initialization(self.conv)
+        self.conv.b.data = self.bias_initialization(self.conv, constant=0)
+
+    def __call__(self, x, train=False):
+        return self.bn(self.conv(x), test=not train)
+
+    def count_parameters(self):
+        return functools.reduce(lambda a, b: a * b, self.conv.W.data.shape)
+
+
+class Conv_BN_ReLU(nutszebra_chainer.Model):
+
+    def __init__(self, in_channel, out_channel, filter_size=(3, 3), stride=(1, 1), pad=(1, 1)):
+        super(Conv_BN_ReLU, self).__init__(
+            conv=L.Convolution2D(in_channel, out_channel, filter_size, stride, pad),
+            bn=L.BatchNormalization(out_channel),
+        )
+        self.in_channel = in_channel
+        self.out_channel = out_channel
+
+    def weight_initialization(self):
+        self.conv.W.data = self.weight_relu_initialization(self.conv)
+        self.conv.b.data = self.bias_initialization(self.conv, constant=0)
+
+    def __call__(self, x, train=False):
+        return F.relu(self.bn(self.conv(x), test=not train))
+
+    def count_parameters(self):
+        return functools.reduce(lambda a, b: a * b, self.conv.W.data.shape)
+
+
+class DoNothing(nutszebra_chainer.Model):
+
+    def __init__(self):
+        super(DoNothing, self).__init__()
+
+    def __call__(self, *args, **kwargs):
+        return args[0]
+
+    def count_parameters(self):
+        return 0
+
+
+class ResNextBlock(nutszebra_chainer.Model):
+
+    def __init__(self, in_channel=64, out_channels=(256, 256, 256), filters=(1, 3, 1), strides=(1, 1, 1), pads=(0, 1, 0), C=32, skip_connection=DoNothing()):
+        super(ResNextBlock, self).__init__()
+        modules = []
+        modules += [('conv_bn_relu_1', Conv_BN_ReLU(in_channel, out_channels[0], filters[0], strides[0], pads[0]))]
+        group_width_in = int(out_channels[0] / C)
+        group_width_out = int(out_channels[1] / C)
+        modules += [('conv_group_2', Group_Conv(group_width_in, group_width_out, filters[1], strides[1], pads[1], C))]
+        modules += [('bn_relu_3', BN_ReLU(out_channels[1]))]
+        modules += [('conv_bn_4', Conv_BN(out_channels[1], out_channels[2], filters[2], strides[2], pads[2]))]
+        # register layers
+        [self.add_link(*link) for link in modules]
+        self.modules = modules
+        self.in_channel = in_channel
+        self.out_channels = out_channels
+        self.filters = filters
+        self.strides = strides
+        self.pads = pads
+        self.C = C
+        self.group_width_in = group_width_in
+        self.group_width_out = group_width_out
+        self.skip_connection = skip_connection
+
+    def weight_initialization(self):
+        [link.weight_initialization() for _, link in self.modules]
+
+    def count_parameters(self):
+        return int(np.sum([link.count_parameters() for _, link in self.modules]))
+
+    def __call__(self, x, train=False):
+        h = self.conv_bn_relu_1(x, train=train)
+        h = self.conv_group_2(h, train=train)
+        h = self.bn_relu_3(h, train=train)
+        h = self.conv_bn_4(h, train=train)
+        x = h + self.skip_connection(x, train=train)
+        return F.relu(x)
+
+
+class ResNext(nutszebra_chainer.Model):
+
+    def __init__(self, category_num, block_num=(3, 4, 23, 3), C=64, d=4):
+        super(ResNext, self).__init__()
+        # conv
+        modules = [('conv_bn_relu', Conv_BN_ReLU(3, 64, 7, 2, 3))]
+        out_channels = [(C * d * i, C * d * i, C * d * i) for i in [2 ** x for x in six.moves.range(len(block_num))]]
+        in_channel = 64
+        for i, n in enumerate(block_num):
+            for ii in six.moves.range(n):
+
+                if i >= 1 and ii == 0:
+                    strides = (1, 2, 1)
+                    skip_connection = Conv_BN(in_channel, out_channels[i][-1], 1, 2, 0)
+                    modules += [('skip_connection_{}'.format(i), skip_connection)]
+                elif i == 0 and ii == 0:
+                    strides = (1, 1, 1)
+                    skip_connection = Conv_BN(in_channel, out_channels[i][-1], 1, 1, 0)
+                    modules += [('skip_connection_{}'.format(i), skip_connection)]
+                else:
+                    strides = (1, 1, 1)
+                    skip_connection = DoNothing()
+                modules += [('resnext_block_{}_{}'.format(i, ii), ResNextBlock(in_channel, out_channels[i], (1, 3, 1), strides, (0, 1, 0), C, skip_connection=skip_connection))]
+                in_channel = out_channels[i][-1]
+        modules += [('linear', Linear(in_channel, category_num))]
+        # register layers
+        [self.add_link(*link) for link in modules]
+        self.modules = modules
+        self.category_num = category_num
+        self.C = C
+        self.block_num = block_num
+        self.n = len(block_num)
+        self.name = 'ResNext_{}_{}'.format(category_num, C)
+
+    def weight_initialization(self):
+        [link.weight_initialization() for _, link in self.modules]
+
+    def count_parameters(self):
+        return int(np.sum([link.count_parameters() for _, link in self.modules]))
+
+    def __call__(self, x, train=False):
+        h = self.conv_bn_relu(x, train=train)
+        h = F.max_pooling_2d(h, (3, 3), (2, 2), (1, 1))
+        for i, n in enumerate(self.block_num):
+            for ii in six.moves.range(n):
+                h = self['resnext_block_{}_{}'.format(i, ii)](h, train=train)
+        batch, channels, height, width = h.data.shape
+        h = F.reshape(F.average_pooling_2d(h, (height, width)), (batch, channels))
+        return self.linear(h, train)
+
+    def calc_loss(self, y, t):
+        loss = F.softmax_cross_entropy(y, t)
+        return loss
+
+    def accuracy(self, y, t, xp=np):
+        y.to_cpu()
+        t.to_cpu()
+        indices = np.where((t.data == np.argmax(y.data, axis=1)) == True)[0]
+        accuracy = defaultdict(int)
+        for i in indices:
+            accuracy[t.data[i]] += 1
+        indices = np.where((t.data == np.argmax(y.data, axis=1)) == False)[0]
+        false_accuracy = defaultdict(int)
+        false_y = np.argmax(y.data, axis=1)
+        for i in indices:
+            false_accuracy[(t.data[i], false_y[i])] += 1
+        return accuracy, false_accuracy
