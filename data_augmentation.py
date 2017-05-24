caf00df98d0aa3096be8993b29703cfab853fdfc
nutszebra <nutszebra@gmail.com>
2017-05-25 01:50:36 +0900

    changed order

--- data_augmentation.py
+++ data_augmentation.py
@@ -10,23 +10,17 @@ def reset(func):
         return func(self, *args, **kwargs)
     return wrapper
 
-    @staticmethod
-    @reset
-    def test(img):
-        da(img).convert_to_image_format(1.0).resize_image_randomly(1.0, size_range=(406, 406), interpolation='bilinear').normalize_picture(1.0, value=10.).convert_to_chainer_format(1.0)
-        return da.x, da.info
-
 
 class DataAugmentationNormalizeBigger(object):
 
     @staticmethod
     @reset
     def train(img):
-        da.load_picture(img).resize_image_randomly(1.0, size_range=(224, 288)).crop_picture_randomly(1.0, sizes=(224, 224)).scale_to_one(1.0, constant=255.).fixed_normalization(1.0, each_rgb=True).horizontal_flipping(0.5).convert_to_chainer_format(1.0)
+        da.load_picture(img).resize_image_randomly(1.0, size_range=(224, 288)).scale_to_one(1.0, constant=255.).fixed_normalization(1.0, each_rgb=True).crop_picture_randomly(1.0, sizes=(224, 224)).horizontal_flipping(0.5).convert_to_chainer_format(1.0)
         return da.x, da.info
 
     @staticmethod
     @reset
     def test(img):
-        da.load_picture(img).resize_image_randomly(1.0, size_range=(256, 256), interpolation='bicubic').crop_center(sizes=(224, 224)).scale_to_one(1.0, constant=255.).fixed_normalization(1.0, each_rgb=True).convert_to_chainer_format(1.0)
+        da.load_picture(img).resize_image_randomly(1.0, size_range=(256, 256), interpolation='bicubic').scale_to_one(1.0, constant=255.).fixed_normalization(1.0, each_rgb=True).crop_center(sizes=(224, 224)).convert_to_chainer_format(1.0)
         return da.x, da.info
